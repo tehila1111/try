0e70bb9eb42c1a684681910efa18c28395ac5e5e
tehila1111 <116339153+tehila12@users.noreply.github.com>
2024-01-16 02:04:56 +0200

    Initial commit1

---
+++ turns/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AppointmentGenerator from './generate';
+
+function App() {
+  return (
+    <div className="App">
+    <AppointmentGenerator/>
+    </div>
+  );
+}
+
+export default App;
